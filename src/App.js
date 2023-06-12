cdc78de8cd412eee06a9f26dde27ff77840674fd
Sayan Das <sayandas001s@gmail.com>
2023-06-12 10:21:43 +0530

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FacialAnalysis from './FacialAnalysis';
 import './App.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? 'dark' : ''}`}>
+      <div className="toggle-container">
+        <span>Toggle Dark Mode</span>
+        <label className="switch">
+          <input type="checkbox" onChange={handleToggleDarkMode} checked={isDarkMode} />
+          <span className="slider round"></span>
+        </label>
+      </div>
       <FacialAnalysis />
     </div>
   );
