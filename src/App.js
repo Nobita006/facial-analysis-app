e47676a6f6806624d09b50c370f9a498df16b068
Sayan Das <sayandas001s@gmail.com>
2024-08-18 01:15:13 +0530

    Removed DB and Updated requirment

--- src/App.js
+++ src/App.js
@@ -11,14 +11,24 @@ function App() {
 
   return (
     <div className={`App ${isDarkMode ? 'dark' : ''}`}>
-      <div className="toggle-container">
-        <span>Toggle Dark Mode</span>
-        <label className="switch">
-          <input type="checkbox" onChange={handleToggleDarkMode} checked={isDarkMode} />
-          <span className="slider round"></span>
-        </label>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div className="toggle-container d-flex justify-content-end align-items-center">
+              <span className="mr-2">Toggle Dark Mode</span>
+              <label className="switch">
+                <input type="checkbox" onChange={handleToggleDarkMode} checked={isDarkMode} />
+                <span className="slider round"></span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <FacialAnalysis />
+          </div>
+        </div>
       </div>
-      <FacialAnalysis />
     </div>
   );
 }
